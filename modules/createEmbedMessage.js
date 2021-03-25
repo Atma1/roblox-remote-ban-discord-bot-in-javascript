b78299cc088cdfd23a7ae65840cbb2a379daa78a
Atma1 <atmasusanto08@gmail.com>
2021-03-25 17:17:05 +0800

    moved the file agan

--- modules/createEmbedMessage.js
+++ modules/createEmbedMessage.js
@@ -4,13 +4,13 @@ const { getUserImg } = require('./getUserImg');
 module.exports = {
 	newEmbedBanInfo: async (bannedAt, bannedBy, playerName, playerID, banReason) => {
 		const playerImg = await getUserImg(playerID);
-		embedMessage.setColor('RED');
+		embedMessage.setColor('DARK_NAVY');
 		embedMessage.setTitle(`Ban Info For Player ${playerName}`);
 		embedMessage.setThumbnail(playerImg);
 		embedMessage.addFields(
-			{ name: 'playerName', value: `${playerName}` },
+			{ name: 'playerName', value: `${playerName}`, inline: true },
 			{ name: 'playerId', value: `${playerID}` },
-			{ name: 'banReason', value: `${banReason}` },
+			{ name: 'banReason', value: `${banReason}`, inline: true },
 			{ name: 'bannedBy', value: `${bannedBy}` },
 			{ name: 'bannedAt', value: `${bannedAt}` },
 		);
