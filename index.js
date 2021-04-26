5ab456dad6a2972dccbb90b6d66fe96db8dbdba6
Atma1 <atmasusanto08@gmail.com>
2021-04-26 11:46:48 +0800

    made fb app init done from client class

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const serviceAccount = require('./serviceAccount.json');
-const DiscordClient = require('./util/DiscordClient');
+const DiscordClient = require('./util/DiscordClient.js');
 require('dotenv').config();
 const token = process.env.token;
 
@@ -8,5 +8,4 @@ const client = new DiscordClient(token, serviceAccount);
 client.cooldowns = new Discord.Collection();
 client.commands = new Discord.Collection();
 
-client.initFireBaseFireStoreApp();
 client.startBot();
