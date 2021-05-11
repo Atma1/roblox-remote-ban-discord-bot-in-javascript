d1042a40992c160776062b248974b01aac4e3342
Atma1 <atmasusanto08@gmail.com>
2021-05-11 11:03:14 +0800

    changed to extended from commandclass

--- util/DataBaseRelatedCommandClass.js
+++ util/DataBaseRelatedCommandClass.js
@@ -2,21 +2,13 @@ const admin = require('firebase-admin');
 const { getUserId } = require('../modules/getUserId');
 const { getUserImg } = require('../modules/getUserImg') ;
 const dateformat = require('dateformat');
+const genericCommandClass = require('./CommandClass');
 const { firestore } = admin;
 
-module.exports = class DataBaseRelatedCommandClass {
+module.exports = class DataBaseRelatedCommandClass extends genericCommandClass {
 
 	constructor(name, desc, usage, commandOptions = {}) {
-		this.name = name;
-		this.desc = desc;
-		this.usage = usage;
-		this.aliases = commandOptions.aliases || false;
-		this.example = commandOptions.example || 'No example';
-		this.cooldown = commandOptions.cooldown || 3;
-		this.args = commandOptions.args || false;
-		this.guildonly = commandOptions.guildonly || false;
-		this.permission = commandOptions.permission || false;
-		this.reqarglength = commandOptions.reqarglength || false;
+		super(name, desc, usage, commandOptions);
 		this.FieldValue = firestore.FieldValue;
 		this.dataBase = admin.firestore();
 	}
