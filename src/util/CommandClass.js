268f2cf7ba1f373b25a72ee60fd8fc1fa69a50b7
Atma1 <atmasusanto08@gmail.com>
2021-05-17 11:15:11 +0800

    added bot client property

---
+++ src/util/CommandClass.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+module.exports = class Command {
+	constructor(botClient, name, desc, usage, commandOptions = {}) {
+		this.name = name;
+		this.desc = desc;
+		this.usage = usage;
+		this.botClient = botClient;
+		this.prefix = process.env.prefix;
+		this.aliases = commandOptions.aliases || false;
+		this.example = commandOptions.example || 'No example';
+		this.cooldown = commandOptions.cooldown || 3;
+		this.args = commandOptions.args || false;
+		this.guildonly = commandOptions.guildonly || false;
+		this.permission = commandOptions.permission || false;
+		this.reqarglength = commandOptions.reqarglength || false;
+	}
+};
