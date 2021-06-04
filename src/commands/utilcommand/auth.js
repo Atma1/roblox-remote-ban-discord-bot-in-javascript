bd54ebb452b8746df9c86eafbb0ce71c453b5528
Atma1 <atmasusanto08@gmail.com>
2021-06-04 11:52:05 +0800

    bug repairs

--- src/commands/utilcommand/auth.js
+++ src/commands/utilcommand/auth.js
@@ -1,12 +1,12 @@
-const DataBaseRelatedCommandClass = require('../../util/DataBaseRelatedCommandClass');
+const DataBaseRelatedCommandClass = require('@util/DataBaseRelatedCommandClass');
 
-module.exports = class extends DataBaseRelatedCommandClass {
+module.exports = class AuthorizeCommand extends DataBaseRelatedCommandClass {
 	constructor(botClient) {
 		super(
 			botClient,
 			'authorizerole',
 			'authorize specific role to command that require permission',
-			'authorizerole @role', {
+			'<@role>', {
 				aliases: ['auth', 'permit', 'authforrole'],
 				example: 'auth @joemama',
 				cooldown: 5,
@@ -18,24 +18,32 @@ module.exports = class extends DataBaseRelatedCommandClass {
 	async execute(msg, args) {
 		const [role] = args;
 		const roleId = role.match(/[0-9]\d+/g);
-		const { cachedAuthorizedRoles } = this.botClient;
+		const { guildConfig, id } = msg.guild;
+		const cachedAuthorizedRoles = guildConfig.get('authorizedRoles');
 
-		if (!msg.guild.roles.cache.find(guildRole => guildRole.id === `${roleId}`)) {
-			return msg.channel.send('Make sure you input the correct role.');
+		if (!roleId) {
+			return msg.reply('That is not a role Id!');
+		}
+
+		if (!msg.guild.roles.cache.find(guildRole => guildRole.id == roleId)) {
+			return msg.reply('Make sure you input the correct role.');
 		}
 
 		try {
-			await this.dataBase.collection('serverDataBase').doc('serverData')
+			await this.dataBase
+				.collection(`guildDataBase:${id}`)
+				.doc('guildConfigurations')
 				.update({
-					authorizedRoles: this.firestore.FieldValue.arrayUnion(`${roleId}`),
+					'guildConfig.authorizedRoles': this.firestore.FieldValue.arrayUnion(`${roleId}`),
 				});
 		}
 		catch (error) {
 			console.error(error);
-			return msg.channel.send(`There was an error while adding the role!\n${error}`);
+			return msg.reply(`There was an error while adding the role!\n${error}`);
 		}
 
 		cachedAuthorizedRoles.push(roleId);
+		guildConfig.set('authorizedRoles', cachedAuthorizedRoles);
 		return msg.channel.send(`${role} has been authorized to use permission restricted command!`);
 	}
 };
--- src/commands/utilcommand/unauth.js
+++ src/commands/utilcommand/unauth.js
@@ -1,12 +1,12 @@
-const DataBaseRelatedCommandClass = require('../../util/DataBaseRelatedCommandClass');
+const DataBaseRelatedCommandClass = require('@util/DataBaseRelatedCommandClass');
 
-module.exports = class extends DataBaseRelatedCommandClass {
+module.exports = class UnauthorzieCommand extends DataBaseRelatedCommandClass {
 	constructor(botClient) {
 		super(
 			botClient,
 			'unauthorizerole',
 			'unauthorize specific role to command that require permission assuming the role exits in the database',
-			'unauthorizerole @role', {
+			'<@role>', {
 				aliases: ['unauth', 'revoke', 'ut'],
 				example: 'unauthorizerole @joemama',
 				cooldown: 5,
@@ -18,28 +18,37 @@ module.exports = class extends DataBaseRelatedCommandClass {
 	async execute(msg, args) {
 		const [role] = args;
 		const roleId = role.match(/[0-9]\d+/g);
-		const { cachedAuthorizedRoles } = this.botClient;
+		const { guildConfig, id } = msg.guild;
+		const cachedAuthorizedRoles = guildConfig.get('authorizedRoles');
+
+		if (!roleId) {
+			return msg.reply('That is not a role Id!');
+		}
 
 		if (!msg.guild.roles.cache.find(guildRole => guildRole.id == roleId)) {
-			return msg.channel.send('Make sure you input the correct role.');
+			return msg.reply('Make sure you input the correct role.');
 		}
 
 		try {
-			await this.dataBase.collection('serverDataBase').doc('serverData')
+			await this.dataBase
+				.collection(`guildDataBase:${id}`)
+				.doc('guildConfigurations')
 				.update({
-					authorizedRoles: this.firestore.FieldValue.arrayRemove(`${roleId}`),
+					'guildConfig.authorizedRoles': this.firestore.FieldValue.arrayRemove(`${roleId}`),
 				});
 		}
 		catch (error) {
 			console.error(error);
-			return msg.channel.send(`There was an error while removing the role!\n${error}`);
+			return msg.reply(`There was an error while removing the role!\n${error}`);
 		}
 
-		for (const storedRoleIdKey of cachedAuthorizedRoles) {
-			if (cachedAuthorizedRoles[storedRoleIdKey] == roleId) {
-				cachedAuthorizedRoles.splice(storedRoleIdKey, 1);
+		for (const cachedRole of cachedAuthorizedRoles) {
+			if (cachedAuthorizedRoles[cachedRole] == roleId) {
+				cachedAuthorizedRoles.splice(cachedRole, 1);
 			}
 		}
+
+		guildConfig.set('authorizedRoles', cachedAuthorizedRoles);
 		return msg.channel.send(`${role} has been restricted to use permission restricted command!`);
 	}
 };
