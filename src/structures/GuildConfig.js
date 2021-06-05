768cc23cc9521b58f8af14d5149eb9a58baab667
Atma1 <atmasusanto08@gmail.com>
2021-06-05 13:06:52 +0700

    added return statment, beautify file

--- src/structures/GuildConfig.js
+++ src/structures/GuildConfig.js
@@ -1,5 +1,10 @@
-const { Structures, Collection } = require('discord.js');
-const { GuildConfigDocConverter } = require('@util/util');
+const {
+	Structures,
+	Collection,
+} = require('discord.js');
+const {
+	GuildConfigDocConverter,
+} = require('@util/util');
 const admin = require('firebase-admin');
 
 Structures.extend('Guild', (Guild) => {
@@ -16,11 +21,14 @@ Structures.extend('Guild', (Guild) => {
 				const snap = await this.getGuildPrefixAndRole();
 
 				if (!snap.exists) {
-					console.warn(`Guild config for guild ${this.id} is not avaible!`);
+					return console.warn(`Guild config for guild ${this.id} is not avaible!`);
 				}
 
-				const { defaultPrefix, authorizedRoles } = snap.data();
-				console.log(defaultPrefix, authorizedRoles);
+				const {
+					defaultPrefix,
+					authorizedRoles,
+				} = snap.data();
+
 				this.guildConfig.set('defaultPrefix', defaultPrefix || '!');
 				this.guildConfig.set('authorizedRoles', authorizedRoles || []);
 			}
