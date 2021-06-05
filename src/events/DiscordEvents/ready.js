f973602e9ef2c781db74ecdbd88f7be765d94887
Atma1 <atmasusanto08@gmail.com>
2021-06-05 16:34:24 +0700

    continued psudeo auto unban

--- src/events/DiscordEvents/ready.js
+++ src/events/DiscordEvents/ready.js
@@ -11,19 +11,32 @@ module.exports = class ReadyEvent extends EventClass {
 			'once',
 		);
 	}
-	execute() {
+	async execute() {
 		console.log(`${this.botClient.user.tag} is ready!`);
 
-		const checkBan = async (guildId) => {
-			const now = Date.now();
+		const checkBan = async () => {
+			// const now = Date.now();
 			const playersToUnban = await fireStore
-				.collection(`guildDataBase:${guildId}`)
+				.collection('serverDataBase')
 				.doc('banList')
 				.collection('bannedPlayerList')
-				.where('banDetails.bannedUntil', '<=', now)
+				.where('banDetails.banReason', '==', 'tesuto')
 				.get();
 			return playersToUnban;
 		};
-		checkBan();
+
+		const unbanSnapShot = await checkBan();
+
+		if (!unbanSnapShot.empty) {
+			// IMPLEMENT STAGED DELETE IF SIZE >= 500
+			console.log(unbanSnapShot.size);
+			const unbanBatch = fireStore.batch();
+
+			unbanSnapShot.forEach(doc => {
+				console.log(`Unbanned ${doc.id}.`);
+				unbanBatch.delete(doc.ref);
+			});
+			await unbanBatch.commit();
+		}
 	}
 };
