fb560dc6ed24aed703cf98dbd9a8768fbbfe1db1
Atma1 <atmasusanto08@gmail.com>
2021-06-12 10:45:49 +0700

    removed trycatch and guilddb, added else statement

--- src/structures/GuildConfig.js
+++ src/structures/GuildConfig.js
@@ -3,27 +3,28 @@ const {
 	Collection,
 } = require('discord.js');
 const {
-	GuildConfigDocConverter,
+	guildConfigDocConverter,
 } = require('@util/util');
 const admin = require('firebase-admin');
+const firestore = admin.firestore();
+
 
 Structures.extend('Guild', (Guild) => {
 	class GuildConfig extends Guild {
 		constructor(client, data) {
 			super(client, data);
-			this.guildDataBase = admin.firestore();
 			this.guildConfig = new Collection();
 			this.setupGuildConfig();
 		}
 
 		async setupGuildConfig() {
-			try {
-				const snap = await this.getGuildPrefixAndRole();
+			const snap = await this.getGuildPrefixAndRole()
+				.catch(err => console.error(err));
 
-				if (!snap.exists) {
-					return console.warn(`Guild config for guild ${this.id} is not avaible!`);
-				}
-
+			if (!snap.exists) {
+				console.warn(`Guild config for guild ${this.id} is not avaible!`);
+			}
+			else {
 				const {
 					defaultPrefix,
 					authorizedRoles,
@@ -32,17 +33,19 @@ Structures.extend('Guild', (Guild) => {
 				this.guildConfig.set('defaultPrefix', defaultPrefix || '!');
 				this.guildConfig.set('authorizedRoles', authorizedRoles || []);
 			}
-			catch (error) {
-				console.error(error);
-			}
 		}
 
 		getGuildPrefixAndRole() {
-			return this.guildDataBase
-				.collection(`guildDataBase:${this.id}`)
-				.doc('guildConfigurations')
-				.withConverter(GuildConfigDocConverter)
-				.get();
+			try {
+				return firestore
+					.collection(`guildDataBase:${this.id}`)
+					.doc('guildConfigurations')
+					.withConverter(guildConfigDocConverter)
+					.get();
+			}
+			catch (error) {
+				throw(`${error}`);
+			}
 		}
 	}
 	return GuildConfig;
