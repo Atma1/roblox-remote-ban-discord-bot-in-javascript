6fa477ff18a0256bf8f043fc224bb71bd8d184d1
Atma1 <atmasusanto08@gmail.com>
2021-06-12 10:43:22 +0700

    refactor

--- src/events/DiscordEvents/guildCreate.js
+++ src/events/DiscordEvents/guildCreate.js
@@ -1,8 +1,7 @@
 const admin = require('firebase-admin');
 const DB = admin.firestore();
-const EventClass = require('@util/EventClass');
-const GuildConfigDocument = require('@util/GuildConfigDocumentClass');
-const { GuildConfigDocConverter } = require('@util/util');
+const EventClass = require('@class/EventClass');
+const { createNewGuildDataBase } = require('@util/util');
 
 module.exports = class GuildCreateEvent extends EventClass {
 	constructor(botClient) {
@@ -17,21 +16,8 @@ module.exports = class GuildCreateEvent extends EventClass {
 		const { id, ownerID } = guildData;
 		const guildOwner = await this.botClient.users.fetch(ownerID);
 
-		try {
-			await DB.collection(`guildDataBase:${id}`)
-				.doc('guildConfigurations')
-				.withConverter(GuildConfigDocConverter)
-				.create(new GuildConfigDocument);
-
-			let dm = 'Collection containing your guild config has been created.';
-			dm += ' Be sure to add roles is authorized to use the commands!';
-
-			guildOwner.send(dm);
-			console.log(`Database for guild ${id} has been created.`);
-		}
-		catch (error) {
-			guildOwner.send('There was an error while creating your guild collection!');
-			console.error(error);
-		}
+		const response = await createNewGuildDataBase(id, DB);
+		guildOwner.send(response)
+			.catch(err => console.error(err));
 	}
 };
