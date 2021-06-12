0563e06124ce0ef8c9550b6dc254c2a362835b97
Atma1 <atmasusanto08@gmail.com>
2021-06-12 10:36:42 +0700

    placed initapp before requiring client

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require('module-alias/register');
+require('dotenv').config();
 const admin = require('firebase-admin');
 const serviceAccount = require('@serviceAccount');
-const BotClient = require('@util/BotClient');
-require('dotenv').config();
-const token = process.env.token;
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
 });
 
+const BotClient = require('@class/BotClient');
+const token = process.env.token;
 const client = new BotClient(token);
+
 client.startBot();
