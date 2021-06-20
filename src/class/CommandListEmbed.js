d83cb8f8cbe1864f313739e7660c7be4e729bc96
Atma1 <atmasusanto08@gmail.com>
2021-06-20 16:09:47 +0700

    commands instead of command

--- src/class/CommandListEmbed.js
+++ src/class/CommandListEmbed.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require('discord.js');
 module.exports = class CommandListEmbed extends MessageEmbed {
 	constructor(commands, prefix) {
 		super();
-		this.setTitle('Commands List');
+		this.setTitle('Commands List:');
 		this.addField(
 			'Important to Know❗', `If you want info on specific command send ${prefix}help [commandName] and don't send it here!`,
 		);
