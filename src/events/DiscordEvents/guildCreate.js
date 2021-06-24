a44659fec414f7bc64a53a27d0f0284420c4c311
Atma1 <atmasusanto08@gmail.com>
2021-06-24 09:57:18 +0700

    initiating guild done from guildconfig instead

--- src/events/DiscordEvents/guildCreate.js
+++ src/events/DiscordEvents/guildCreate.js
@@ -1,7 +1,4 @@
-const admin = require('firebase-admin');
-const DB = admin.firestore();
 const EventClass = require('@class/EventClass');
-const { createNewGuildDataBase } = require('@util/util');
 
 module.exports = class GuildCreateEvent extends EventClass {
 	constructor(botClient) {
@@ -12,16 +9,6 @@ module.exports = class GuildCreateEvent extends EventClass {
 		);
 	}
 	async execute(guildData) {
-
-		const { id, ownerID } = guildData;
-		const guildOwner = await this.botClient.users.fetch(ownerID);
-
-		try {
-			const response = await createNewGuildDataBase(id, DB);
-			guildOwner.send({ content:response });
-		}
-		catch (error) {
-			console.error(error);
-		}
+		console.log(`This bot has been added to guild with an ID of ${guildData.id}`);
 	}
 };
