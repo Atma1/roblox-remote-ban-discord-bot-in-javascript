71ed09fc873ae0d744b8c4d6569e8df002ed195d
Atma1 <atmasusanto08@gmail.com>
2021-07-06 17:42:43 +0800

    minor change

--- src/commands/bancommand/tempban.js
+++ src/commands/bancommand/tempban.js
@@ -64,7 +64,7 @@ module.exports = class TempBanCommand extends DataBaseRelatedCommandClass {
 		}
 		catch (error) {
 			console.error(error);
-			return message.reply({ content:`There was an error while banning the player!\n${error}`, allowedMentions: { repliedUser: true } });
+			return message.reply({ content:`there was an error while banning the player!\n${error}`, allowedMentions: { repliedUser: true } });
 		}
 	}
 };
