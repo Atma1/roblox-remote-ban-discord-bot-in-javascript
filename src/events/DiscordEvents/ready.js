d11c1f807ef59281aeabcca08c23ec21892319ef
Atma1 <atmasusanto08@gmail.com>
2021-07-13 10:58:20 +0800

    imported runautoban module

--- src/events/DiscordEvents/ready.js
+++ src/events/DiscordEvents/ready.js
@@ -1,7 +1,7 @@
 const EventClass = require('@class/EventClass');
-const admin = require('firebase-admin');
-const readableToMs = require('readable-to-ms');
-const firestore = admin.firestore();
+const runAutoUnban = require('@modules/runAutoUnbaan');
+const ms = require('ms');
+
 
 module.exports = class ReadyEvent extends EventClass {
 	constructor(botClient) {
@@ -14,54 +14,9 @@ module.exports = class ReadyEvent extends EventClass {
 	async execute() {
 		console.log(`${this.botClient.user.tag} is ready!`);
 
-		function runAutoUnban(client) {
-			const clientGuild = client.guilds.cache;
-			clientGuild.forEach(async guild => {
-				await checkBan(guild.id);
-			});
-
-			setTimeout(() => {
-				runAutoUnban(client);
-			}, readableToMs('10m'));
-		}
-
-		async function checkBan(guildId) {
-			try {
-				const querySnapshot = await fetchBanDocument(guildId);
-
-				if (querySnapshot.empty) return;
-
-				await batchDelete(querySnapshot);
-
-				process.nextTick(() => {
-					checkBan(guildId);
-				});
-			}
-			catch (error) {
-				console.error(error);
-			}
-		}
-
-		function fetchBanDocument(guildId) {
-			const now = Date.now();
-			return firestore
-				.collection(`guildDataBase:${guildId}`)
-				.doc('banList')
-				.collection('bannedPlayerList')
-				.where('banDetails.bannedUntil', '<=', now)
-				.limit(500)
-				.get();
-		}
-
-		async function batchDelete(snapshot) {
-			const batch = firestore.batch();
-			snapshot.forEach(doc => {
-				console.log(`Deleted ${doc.id}.`);
-				batch.delete(doc.ref);
-			});
-			await batch.commit();
-		}
-
 		runAutoUnban(this.botClient);
+		setTimeout(() => {
+			runAutoUnban(this.botClient);
+		}, ms('10m'));
 	}
 };
