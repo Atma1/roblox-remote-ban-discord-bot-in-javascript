774e9af1776b399c1bbc8ddeb8c992d00697ca0a
Atma1 <atmasusanto08@gmail.com>
2021-08-03 15:00:02 +0800

    changed the behavior of setupcommandpermission

--- src/modules/guildCommandPermission.js
+++ src/modules/guildCommandPermission.js
@@ -1,63 +1,28 @@
-const { guildConfigDocConverter, createNewGuildDataBase } = require('@util/util');
-const admin = require('firebase-admin');
-const RolePermissionData = require('@class/bar');
-const UserPermissionData = require('@class/foo');
-const CommandPermission = require('@class/foobar');
-const firestore = admin.firestore();
+const PermissionData = require('@class/Permission Data/PermissionData');
+const CommandPermission = require('@class/Permission Data/CommandPermission');
 
-function setupCommandPermission(slashCommands, guilds, botClient) {
-	const slashCommandIds = slashCommands.map(cmd => cmd.id);
-	guilds.forEach(async guild => {
-		try {
-			const { id:guildId, ownerId } = guild;
-			const snapshot = await fetchGuildPrefixAndRole(guildId);
-			let fullPermissions;
-			if (!snapshot.exists) {
-				const [response, guildOwner] = await Promise.all([
-					createNewGuildDataBase(guildId, firestore),
-					botClient.users.fetch(this.ownerID),
-				]);
-				const userPermissionData = new UserPermissionData(ownerId);
-				fullPermissions = slashCommandIds.map(commandId => new CommandPermission(commandId, userPermissionData));
-				directMessageGuildOwner(guildOwner, response);
-			}
-			else {
-				const { authorizedRoles } = snapshot.data();
-				if (authorizedRoles.length) {
-					const authorizedRolesDatas = authorizedRoles.map(role => new RolePermissionData(role));
-					fullPermissions = slashCommandIds.map(commandId => new CommandPermission(commandId, authorizedRolesDatas));
-				}
-				else {
-					const userPermissionData = new UserPermissionData(ownerId);
-					fullPermissions = slashCommandIds.map(commandId => new CommandPermission(commandId, userPermissionData));
-				}
-				await guild.commands?.permission.set({ fullPermissions });
-			}
-		}
-		catch (error) {
-			console.error;
-		}
-	});
+async function setupCommandPermission(guildConfig, guild) {
+	const { ownerId } = guild;
+	let authorizedPermission;
+	const authorizedRolesArray = guildConfig.get('authorizedRoles');
+	const slashCommandIds = guildConfig.get('guildSlashCommandIds');
+	if (!authorizedRolesArray.length) {
+		const userPermissionData = new PermissionData(ownerId, 'USER', true);
+		authorizedPermission = slashCommandIds.map(commandId => new CommandPermission(commandId, userPermissionData));
+	}
+	else {
+		authorizedPermission = authorizedRolesArray.map(roleId => new PermissionData(roleId, 'ROLE', true));
+		authorizedPermission.push(new PermissionData(ownerId, 'USER', true));
+	}
+	await updateSlashCommandPermission(guild, authorizedPermission, slashCommandIds);
 }
 
-function directMessageGuildOwner(guildOwner, message) {
-	guildOwner.send({ content:message });
-}
-
-function getGuildConfigCollection(guildId, botClient) {
-	const { guildConfig } = botClient;
-	return guildConfig.get(guildId);
-}
-
-function fetchGuildPrefixAndRole(guildId) {
-	return firestore
-		.collection(`guildDataBase:${guildId}`)
-		.doc('guildConfigurations')
-		.withConverter(guildConfigDocConverter)
-		.get();
+async function updateSlashCommandPermission(guild, authorizedPermission, slashCommandIds) {
+	const fullPermissions = slashCommandIds.map(commandId => new CommandPermission(commandId, authorizedPermission));
+	await guild.commands?.permissions.set({ fullPermissions });
 }
 
 module.exports = {
-	getGuildConfigCollection: getGuildConfigCollection,
-	setupGuildConfig: setupCommandPermission,
+	updateSlashCommandPermission: updateSlashCommandPermission,
+	setupCommandPermission: setupCommandPermission,
 };
