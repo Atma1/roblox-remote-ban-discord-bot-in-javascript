d4116afa5ec72b0bf976c609e004cf9a74cdfb58
Atma1 <atmasusanto08@gmail.com>
2021-08-09 16:50:39 +0800

    removed unused intent,property rename,new function

--- src/class/BotClient.js
+++ src/class/BotClient.js
@@ -4,7 +4,7 @@ const {
 	Intents,
 } = require('discord.js');
 const util = require('@util/util');
-const botIntents = new Intents(['GUILDS', 'GUILD_MESSAGES', 'GUILD_INTEGRATIONS']);
+const botIntents = new Intents(['GUILDS', 'GUILD_MESSAGES']);
 
 module.exports = class BotClient extends Client {
 
@@ -12,13 +12,13 @@ module.exports = class BotClient extends Client {
 		super({ intents: botIntents });
 		this.token = token;
 		this.cooldowns = new Collection();
-		this.commands = new Collection();
-		this.guildConfig = new Collection();
+		this.guildConfigCollection = new Collection();
+		this.slashCommands = new Collection();
 	}
 
 	startBot() {
 		try {
-			util.loadCommands(this);
+			util.loadSlashCommands(this);
 			util.loadEvents(this);
 			this.login(this.token);
 		}
