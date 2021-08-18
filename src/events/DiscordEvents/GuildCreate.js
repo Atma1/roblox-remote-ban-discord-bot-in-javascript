b2d0bb79e615e22a4b387ee96a3ccafb2dd5701f
Atma1 <atmasusanto08@gmail.com>
2021-08-18 16:40:10 +0800

    updated require path

--- src/events/DiscordEvents/GuildCreate.js
+++ src/events/DiscordEvents/GuildCreate.js
@@ -1,4 +1,4 @@
-const EventClass = require('@class/EventClass');
+const EventClass = require('@class/Event/EventClass');
 const { setupGuildConfig } = require('@modules/GuildConfig');
 
 module.exports = class GuildCreateEvent extends EventClass {
