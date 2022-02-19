66847acb41887c6f848064942dd1a79c8682eefe
Atma1 <atmasusanto38@gmail.com>
2022-02-20 07:59:10 +0800

    fixed a typo

--- src/class/Event/EventClass.js
+++ src/class/Event/EventClass.js
@@ -2,6 +2,6 @@ module.exports = class EventClass {
 	constructor(botClient, eventOptions) {
 		this.botClient = botClient;
 		this.eventType = eventOptions.eventType;
-		this.eventEmmiter = eventOptions.eventEmmiter;
+		this.eventEmitter = eventOptions.eventEmitter;
 	}
 };
