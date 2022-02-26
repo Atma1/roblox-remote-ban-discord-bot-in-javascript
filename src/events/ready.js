1b781a0a5b557ebe2f80a849ae6dcf6dda677657
Atma1 <atmasusanto38@gmail.com>
2022-02-26 18:23:16 +0800

    simplification

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,6 @@
 const EventClass = require('@class/Event/EventClass');
 const runAutoUnban = require('@modules/runAutoUnban');
-const { setupGuildConfigAndCommand } = require('@modules/guildConfig');
+const { setupGuildConfigAndCommandForGuilds } = require('@modules/guildConfig');
 const ms = require('ms');
 
 module.exports = class ReadyEvent extends EventClass {
@@ -14,13 +14,10 @@ module.exports = class ReadyEvent extends EventClass {
 		);
 	}
 	async execute() {
+		const { cache:clientGuildsMap } = this.botClient.guilds;
+		await setupGuildConfigAndCommandForGuilds(clientGuildsMap, this.botClient);
+		await runAutoUnban(clientGuildsMap);
 		console.log(`${this.botClient.user.tag} is ready!`);
-		const { cache:clientGuilds } = this.botClient.guilds;
-
-		clientGuilds.forEach(async guild =>
-			await setupGuildConfigAndCommand(guild, this.botClient));
-
-		runAutoUnban(this.botClient);
-		setTimeout(() => runAutoUnban(clientGuilds), ms('10m'));
+		setTimeout(() => runAutoUnban(clientGuildsMap), ms('10m'));
 	}
 };
